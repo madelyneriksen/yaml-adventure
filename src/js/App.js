2f37167b7c33425db885027a73cc52bad2e85da0
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2019-02-20 20:56:25 -0800

    Added game to higher order app component.

--- src/js/App.js
+++ src/js/App.js
@@ -1,27 +1,12 @@
 import React from 'react';
-import TextBox from './game/text-box';
-import GameButton from './game/button';
-import ButtonHolder from './game/button-holder';
+import sample_game from '../example/example.yaml';
+import Game from './game';
 
 
 export default () => {
-  const text = `
-Welcome to Yaml Adventure!
-
-Create your adventure file and save it as a yaml.
-  `;
-  const clickCallback = (value) => {
-    return (event) => {
-      console.log(value);
-    }
-  }
   return (
     <React.Fragment>
-      <TextBox text={text} />
-      <ButtonHolder>
-        <GameButton text="Let's Play!" value="getting_started" onClick={clickCallback} />
-      </ButtonHolder>
+      <Game game={sample_game} />
     </React.Fragment>
-
   )
 }
