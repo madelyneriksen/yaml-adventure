dd81922223752d1a087c499abcec2c6f091b1397
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2019-02-23 09:00:01 -0800

    Adjusting GameUploader to be a bit easier to test.

--- src/js/game/game-loader.js
+++ src/js/game/game-loader.js
@@ -1,17 +1,45 @@
 import React from 'react';
-import { load } from 'js-yaml';
+import { safeLoad } from 'js-yaml';
 
 
-export default (props) => {
+export default class GameUploader extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.readUpload = this.readUpload.bind(this);
+    this.parseUpload = this.parseUpload.bind(this);
+  }
+
+  readUpload(event) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = this.parseUpload;
+      reader.readAsText(file);
+    }
+  }
+
+  parseUpload(event) {
+    var raw = String(event.target.result);
+    try {
+      var uploadedGame = safeLoad(raw);
+      this.props.swapGame(uploadedGame);
+    } catch (e) {
+      console.log(e);
+    };
+  };
+
+  render() {
     return (
       <div className="game__uploader">
         <label className="game__uploader__label">
-          <input type="file" className="game__uploader__input" onChange={props.handler} />
+          <input type="file" className="game__uploader__input" onChange={this.readUpload} />
           Upload a Game
         </label>
-        <button className="game__button--secondary" onClick={props.loadDefault}>
+        <button className="game__button--secondary" onClick={this.props.loadDefault}>
           Or load the example game
         </button>
     </div>
     )
+  }
 }
